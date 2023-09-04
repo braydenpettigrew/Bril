32348f5aefb8ed4e7f8bd2dad04e6b96ce69badf
braydenpettigrew <brayden@thepettigrews.org>
2023-09-04 13:07:40 -0400

    initial commit

---
+++ App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Bril 3.0</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#5BBA66',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
